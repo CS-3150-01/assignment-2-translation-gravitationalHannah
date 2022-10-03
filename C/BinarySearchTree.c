110a4bd0bc38dbe283629ad4b9c3c0f3466d31e4
gravitationalHannah <fallsee23@gmail.com>
2022-10-03 17:12:08 -0600

    finished

--- C/BinarySearchTree.c
+++ C/BinarySearchTree.c
@@ -1,5 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    return 0;
+//Represent the node of binary tree
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+//Assign data to the new node, set left and right children to null
+struct Node* add(int z){
+    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+    p->data = z;
+    p->left = NULL;
+    p->right = NULL;
+}
+
+//Represent the root of binary tree 
+void BinarySearchTree(){
+    struct Node* root = NULL;
+}
+//factorial() will calculate the factorial of given number
+int factorial(int num){
+    int fact = 1;
+    if(num==0){
+        return 1;
+    } else{
+        while(num > 1){
+            fact = fact*num;
+            num--;
+        }
+        return fact;
+    }
+};
+
+//numOfBST() will calculate the total number of possible BST by calculating Catalan Number for given key
+int numOfBST(int key){
+    int catalanNumber = factorial(2*key)/(factorial(key + 1) * factorial(key));
+    return catalanNumber;
+}
+
+int main(){
+    BinarySearchTree();
+    printf("Total number of possible Binary Search Trees with given key: %d", numOfBST(5));
 }
--- C/SinglyLinkedList.c
+++ C/SinglyLinkedList.c
@@ -1,5 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+//Represent a node of the singly linked list
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+//Represent the head and tail of the singly linked list 
+struct Node *head = NULL;
+struct Node *tail = NULL;
+
+//addNode() will add a new node to the list
+void addNode (int z){
+    struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+    p->data = z;
+    p->next = NULL;
+
+//If list is empty, both head and tail will point to new node 
+    if(head==NULL){
+        head = p;
+        tail = p;
+    } else{
+        //newNode will be added after tail such that tail's next will point to newNode
+        tail->next = p;
+        //newNode will become new tail of the list  
+        tail = p;
+    }
+}
+
+//display() will display all the nodes present in the list
+void display(struct Node* node){
+    //Node current will point to head
+    struct Node* current = node;
+    if (current==NULL){
+        printf("List is empty.\n");
+        return;
+    }else{
+        printf("Nodes of singly linked list: \n");
+        while(current != NULL){
+            printf("%d ", current->data);
+                current = current->next;
+         }
+         printf("\n");
+         return;
+    }
+}
+
 
 int main() {
+    //Add nodes to the list 
+    addNode(1);
+    addNode(2);
+    addNode(3);
+    addNode(4);
+
+    //Displays the nodes present in the list
+    display(head);
     return 0;
 }
